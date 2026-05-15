a743097a2b2bf8d84384d8c5f0463fd5269deefe
Junaid Nasir <junaidnasirv2@gmail.com>
2026-05-15 13:15:39 +0100

    fix build: set Node 20.x, remove dead code referencing deleted config fields

--- src/utils/bin/summary.ts
+++ src/utils/bin/summary.ts
@@ -1,15 +1,14 @@
 import config from '../../../config.json';
 
 const summary = async (args: string[]): Promise<string> => {
-  if (config.ascii === 'Junaid') {
-    return `                                                  
-             @@@@@@@@@@@@@                   summary:
+  return `                                                  
+             @@@@@@@@@@@@@                   summary:
         @@@@               @@@@             -----------
-      @@                       @@            ABOUT
+      @@                       @@            ABOUT
     @@                           @@         
-  @@                               @@        ${config.name}
+  @@                               @@        ${config.name}
  @@                         @@@     @@      ﰩ ${config.ps1_hostname} 
-@@        @@@                        @@     爵 London, UK
+@@        @@@                        @@     爵 London, UK
 @@                                   @@     -----------
 @@             .@@@@@@@@@@.          @@      CONTACT 
  @@           @@          @@        @@       
@@ -21,26 +20,6 @@ const summary = async (args: string[]): Promise<string> => {
          @|                      @@         
 
 `;
-  } else {
-    return `
-           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  summary
-        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
-      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
-    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-▐▓                                 ▐▓       -----------
-▐▓        > L I V E T E R M        ▐▓        CONTACT 
-▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓       <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-   ▓▓      ▐▓▓\xa0   ▓▓\xa0   ▐▓▓\xa0    ▓▓\xa0
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
-
-`;
-  }
 };
 
 export default summary;
